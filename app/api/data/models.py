b150e9f913a8b8b661528975e96c96c2a1f41e52
Fernando Bustos <lufe.bura@gmail.com>
2024-01-31 15:59:04 -0600

    BugFix: Example cocktail model

--- app/api/data/models.py
+++ app/api/data/models.py
@@ -21,20 +21,20 @@ class Cocktail(BaseModel):
     tags: Optional[List[str]] = Field(None)
     image: Optional[str] = Field(None, max_length=500)
     
-    class Config:
-        schema_extra = {
-            "examples" : [
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
                 {
-                    "id" : 1,
                     "name": "Peach Bellini",
                     "ingredients": ["peach puree", "Prosecco"],
                     "recipet": ["Add peach puree to a chilled champagne flute.", "Top up with Prosecco and stir gently."],
-                    "history" : "a little background of your new cocktail",
+                    "history" : "A little background of your new cocktail",
                     "tags": ["champagne flute", "Prosecco", "brunch"],
                     "image": "https://firebasestorage.googleapis.com/v0/b/mixology-af57f.appspot.com/o/christmas_champagne_cocktail.jpg?alt=media&token=9b7da4d4-cd8d-4a48-97c2-da2836e0d339"
                 }
             ]
         }
+    }
     
 
 #class CocktailAsIngredient(Base):
